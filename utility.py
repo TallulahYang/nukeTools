b08581be8f9f6d271441cc2d7f20700d4357aa3a
jackieliao <jackieliao67@gmail.com>
2016-06-16 17:23:20 -0400

    clean up

--- utility.py
+++ utility.py
@@ -5,16 +5,10 @@ import nukescripts
 def printKnobs():
     print '\n'.join(sorted(nuke.selectedNode().knobs()))
 
-def test():
-    print "hello world"
-
 #convert rgb to hex value
-def getHexColor():
-    print "hello world" 
-    """
+def getHexColor(red,green,blue):
     r = red
     g = green
     b = blue
     hexColor = int('%02x%02x%02x%02x' % (r*255,g*255,b*255,1),16)
     return hexColor
-    """
